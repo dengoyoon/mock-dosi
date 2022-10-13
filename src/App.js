ee6f4834c2227357e07d192c0958cde8becb1e1c
dengoyoon <engus927@naver.com>
2022-10-14 02:01:27 +0900

    Header, Footer 추가

--- src/App.js
+++ src/App.js
@@ -2,21 +2,23 @@ import "./stylesheets/App.scss";
 import React from "react";
 import Home from "./Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import NavBar from "./components/NavBar";
 import Adventure from "./Adventure";
 import Membership from "./Membership";
 import GiftBox from "./GiftBox";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <BrowserRouter>
-      <NavBar />
+      <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/adventure" element={<Adventure />}></Route>
         <Route path="/membership" element={<Membership />}></Route>
         <Route path="/giftbox" element={<GiftBox />}></Route>
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "./NavBar";
+
+function Header() {
+  return (
+    <header className="header">
+      <div>DOSI Citizen</div>
+      <NavBar />
+      <NavBar />
+    </header>
+  );
+}
+
+export default Header;
