badfac667f20431f30f56d46f2f3de0891783a11
dengoyoon <engus927@naver.com>
2022-10-15 02:46:08 +0900

    헤더 바디 퍼블리싱 완료

--- src/components/ExternalNavBar.js
+++ src/components/ExternalNavBar.js
@@ -6,19 +6,19 @@ import "../stylesheets/ExternalNavBar.scss";
 function ExternalNavBar() {
   return (
     <ul className="navbar">
-      <li>
+      <li className="navbar__item">
         <Link>디코</Link>
       </li>
-      <li>
+      <li className="navbar__item">
         <Link>트위터</Link>
       </li>
-      <li>
+      <li className="navbar__item">
         <Link>브런치</Link>
       </li>
-      <li>
+      <li className="navbar__item">
         <Link>프로필</Link>
       </li>
-      <li>
+      <li className="navbar__item">
         <Link>지갑</Link>
       </li>
     </ul>
--- src/components/InternalNavBar.js
+++ src/components/InternalNavBar.js
@@ -1,30 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import "../stylesheets/InternalNavBar.scss";
+
 function InternalNavBar() {
   return (
     <ul className="navbar">
-      <li>
-        <Link>
+      <li className="navbar__item">
+        <Link className="active" to="/">
           <span>Home</span>
         </Link>
       </li>
-      <li>
-        <Link>
+      <li className="navbar__item">
+        <Link to="/adventure">
           <span>Adventure</span>
         </Link>
       </li>
-      <li>
-        <Link>
+      <li className="navbar__item">
+        <Link to="/membership">
           <span>Citizen Membership</span>
         </Link>
       </li>
-      <li>
+      <li className="navbar__item">
         <Link>
           <span>Notice</span>
         </Link>
       </li>
-      <li>
+      <li className="navbar__item">
         <Link>
           <span>Marketplace</span>
         </Link>
