b34cb6ea99499d5c72a260f819036ad7becb01c2
dengoyoon <engus927@naver.com>
2022-10-18 06:24:55 +0900

    카운트 다운까지 퍼블리싱

--- src/components/adventure/AdventureHero.js
+++ src/components/adventure/AdventureHero.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import HeroSwitch from "./HeroSwitch";
+import Countdown from "../countdown/Countdown";
 
 import "../../stylesheets/AdventureHero.scss";
 
@@ -10,7 +10,27 @@ function AdventureHero() {
       <div className="container flex justify-content-center">
         <HeroSwitch />
       </div>
-      <div className="container"></div>
+      <div className="container">
+        <div className="adventure-hero__header">
+          <h3 className="adventure-hero__header__title">Weekly Adventure</h3>
+          <div className="adventure-hero__header__description">
+            <p>Citizen 여러분들을 위해 준비된 보물을 찾아 모험을 떠나보세요!</p>
+            <p>
+              매주 전세계적으로 유명한 &apos;블루칩 NFT&apos;를 포함한 다양한
+              NFT를 보상으로 에어드랍 받을 수 있는 새로운 DOSI
+              Adventure&#40;도시 탐사&#41;가 열립니다.
+            </p>
+          </div>
+          <div className="adventure-hero__header__schedule">
+            <p>참여 기간 : 10.13 ~ 10.20 (GMT+9)</p>
+            <p>당첨자 발표일 : 2022.10.27 (GMT+9)</p>
+          </div>
+        </div>
+        <div className="adventure-hero__body">
+          <Countdown />
+        </div>
+        <div className="adventure-hero__footer"></div>
+      </div>
     </section>
   );
 }
