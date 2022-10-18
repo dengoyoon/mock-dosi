dc8055f8992eabe8f1e68a48696bb88e8486a7e8
dengoyoon <engus927@naver.com>
2022-10-19 00:21:49 +0900

    참여횟수 그림 제외하고 퍼블리싱

--- src/components/adventure/Participations.js
+++ src/components/adventure/Participations.js
@@ -38,7 +38,19 @@ function Participations() {
             </div>
           </div>
         </div>
-        <div className="participations__card"></div>
+        <div className="participations__card">
+          <div className="container">
+            <div className="adventure-progress">
+              <div className="adventure-progress__header">
+                <h5>나의 이번주 참여 횟수</h5>
+                <p>Adventure는 최대 5번 참여할 수 있습니다.</p>
+                <p>참여 횟수가 많아질수록 당첨 확률이</p>
+                <p>높아집니다.</p>
+              </div>
+              <div className="adventure-progress__body"></div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
