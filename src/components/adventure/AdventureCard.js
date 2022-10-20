34ce7f7dba4fc2a46bc2c7774427439aea3a8f70
dengoyoon <engus927@naver.com>
2022-10-20 09:45:13 +0900

    모달 띄우고 끄기 성공

--- src/components/adventure/AdventureCard.js
+++ src/components/adventure/AdventureCard.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactModal from "react-modal";
 
 import "../../stylesheets/AdventureCard.scss";
 
 function AdventureCard({ imgUrl, head, small, isVisibleBody }) {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleOpenModal = () => {
+    setShowModal(true);
+    console.log("open modal", showModal);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    console.log("close modal", showModal);
+  };
+
   return (
     <div className="adventure-card">
-      <div className="overlay">
+      <div className="overlay" onClick={handleOpenModal}>
         {isVisibleBody ? (
           <>
             <div className="adventure-card__adventure-header">
@@ -26,6 +39,18 @@ function AdventureCard({ imgUrl, head, small, isVisibleBody }) {
           </>
         )}
       </div>
+      {
+        <ReactModal
+          isOpen={showModal}
+          contentLabel="Minimal Modal Example"
+          onRequestClose={handleCloseModal}
+          appElement={document.querySelector("body")}
+        >
+          <button type="button" onClick={handleCloseModal}>
+            Close
+          </button>
+        </ReactModal>
+      }
     </div>
   );
 }
