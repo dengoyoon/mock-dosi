9b58f9d75b4a74eb83a9d832b1327a84a5252af6
dengoyoon <engus927@naver.com>
2022-10-21 03:57:30 +0900

    카운트 다운 약간의 오류 있으나 구현 완료

--- src/components/countdown/CountdownCard.js
+++ src/components/countdown/CountdownCard.js
@@ -2,10 +2,12 @@ import React from "react";
 
 import "../../stylesheets/CountdownCard.scss";
 
+const getStringNumber = (number) => (number < 10 ? `0${number}` : `${number}`);
+
 function CountdownCard({ number, label }) {
   return (
     <div className="countdown__card">
-      <div className="countdown__card__body">{number}</div>
+      <div className="countdown__card__body">{getStringNumber(number)}</div>
       <div className="countdown__card__footer">{label}</div>
     </div>
   );
