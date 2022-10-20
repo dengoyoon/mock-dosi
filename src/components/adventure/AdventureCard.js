8c84821750ad4128eb56f9db5da0a74c68fb4ccf
Dengo <engus927@naver.com>
2022-10-21 02:57:58 +0900

    카드 모달 일단 완성하고 마무리
    * 리액트 모달 설치
    
    * 모달 띄우고 끄기 성공
    
    * 카드 모달 일단 완성

--- src/components/adventure/AdventureCard.js
+++ src/components/adventure/AdventureCard.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactModal from "react-modal";
 
 import "../../stylesheets/AdventureCard.scss";
 
 function AdventureCard({ imgUrl, head, small, isVisibleBody }) {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleOpenModal = () => {
+    setShowModal(true);
+    console.log("open modal", showModal);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    console.log("close modal", showModal);
+  };
+
   return (
     <div className="adventure-card">
-      <div className="overlay">
+      <div className="overlay" onClick={handleOpenModal}>
         {isVisibleBody ? (
           <>
             <div className="adventure-card__adventure-header">
@@ -26,6 +39,22 @@ function AdventureCard({ imgUrl, head, small, isVisibleBody }) {
           </>
         )}
       </div>
+      <ReactModal
+        isOpen={showModal}
+        contentLabel="Minimal Modal Example"
+        onRequestClose={handleCloseModal}
+        appElement={document.querySelector("body")}
+        className="adventure-card-modal"
+        overlayClassName="adventure-card-modal-overlay"
+      >
+        <div className="adventure-card-modal__img-frame">
+          <img src={imgUrl} />
+        </div>
+        <div className="adventure-card-modal__body">
+          <h5>{head}</h5>
+        </div>
+        <button type="button" onClick={handleCloseModal}></button>
+      </ReactModal>
     </div>
   );
 }
