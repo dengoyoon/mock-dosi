30ef49ae4cc9a0119d8736dacc6a6eb83ebfda81
dengoyoon <engus927@naver.com>
2022-10-24 21:26:42 +0900

    Welcome User 컴포넌트 생성

---
+++ src/components/home/WelcomUser.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function WelcomeUser() {
+  return (
+    <section className="welcome-user">
+      <div>WelcomeUser</div>
+    </section>
+  );
+}
+
+export default WelcomeUser;
